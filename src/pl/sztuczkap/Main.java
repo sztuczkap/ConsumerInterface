07c135f90db70d60b567c14bf40837100d37789e
sztuczkap <p.sztuczka@wp.pl>
2020-12-29 13:26:43 +0100

    interfejs Consumer

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,41 @@
 package pl.sztuczkap;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.ObjIntConsumer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Consumer<Integer> consumer = System.out::println;
+
+        Consumer<Integer> secondConsumer = consumer.andThen(number -> {
+            for (int i = 0; i < number; i++) {
+                System.out.println("Hello");
+            }
+        });
+//        secondConsumer.accept(4);
+
+
+        BiConsumer<String, Integer> biConsumer = (text, number) -> {
+            System.out.println(text + " " + number);
+        };
+
+//        biConsumer.accept("Hell", 1);
+
+        ObjIntConsumer<String> objIntConsumer = (text, number) -> {
+            System.out.println(text + " " + number);
+        };
+
+//        objIntConsumer.accept("Hi", 2);
+
+        List.of(1, 2, 3, 4).forEach((number) -> System.out.println(number));
+        Map.of(1, "One", 2, "Two", 3, "Three").forEach((num, text) -> {
+            System.out.println(num + " " + text);
+        });
+
     }
 }
